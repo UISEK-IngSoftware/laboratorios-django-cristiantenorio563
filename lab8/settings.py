efc6a196320ab5635fbb84ff32111b869baa98d6
Cristian <cristian.tenorio@uisek.edu.ec>
2026-06-23 11:28:03 -0500

    Laboratorio 10, conexion de laboratorios, axios

--- lab8/settings.py
+++ lab8/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'api',
     'oauth2_provider',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -151,3 +153,5 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ),
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
